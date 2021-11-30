04c23c7a88914f59126cf1254af481f0ad00d9eb
Raghav Mangrola <dev@raghav.io>
2021-11-29 20:04:00 -0800

    gitignore

---
+++ sliding-window/javascript solutions/longest-subarray-with-ones-after-replacement.js
@@ -0,0 +1,8 @@
+const length_of_longest_substring = function(arr, k) {
+    
+    return -1;
+  };
+  
+
+console.log(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2));
+console.log(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3));
